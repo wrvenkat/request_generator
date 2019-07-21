9061ac18e3e037885ba4a992dbd5a312165ce339
Venkat Raman <wrvenkat@gmail.com>
2019-07-21 19:04:51 -0400

    1. Removed sefl.setup() calls from all the elements. 2. Fixed default type usage in Input element. 3. Fixed child text element creation in elements applicable to. 4. Fixed self_closing requirement in Meta element. 5. Added Text element.

--- request_generator/html/simple_html.py
+++ request_generator/html/simple_html.py
@@ -4,6 +4,10 @@ from simple_html_element import SimpleHTMLElement as Element
 Class that houses the various HTML Tags/Elements like form, iframe, input, label etc.
 """
 
+class Text(Element):
+    def __init__(self,text='', parent=None, child=None):
+        super(Text, self).__init__(text=text, parent=parent, child=child)        
+
 class Form(Element):
     def __init__(self,action='', method='GET', attrs=None, parent=None, child=None):
         #the action and method if present in attrs, overrides the one provided/default
@@ -16,25 +20,21 @@ class Form(Element):
             if 'action' not in attrs:
                 attrs['action'] = action
 
-        super(Form, self).__init__('form', attrs=attrs, parent=parent, child=child)
-        self.setup(parent, child)
+        super(Form, self).__init__('form', attrs=attrs, parent=parent, child=child)        
 
 class IFrame(Element):
     def __init__(self, src='', width="500", height="500", attrs=None, parent=None, child=None):
         #attrs in attrs take precedence
-        if attrs is None or len(attrs) == 0:
-            if attrs is None:
-                attrs = {}
+        if attrs is None:
+            attrs = {}
             
-            if 'src' not in attrs:
-                attrs['src'] = src
-            if 'width' not in attrs:
-                attrs['width'] = width
-            if 'height' not in attrs:
-                attrs['height'] = height
-
+        if 'src' not in attrs:
+            attrs['src'] = src
+        if 'width' not in attrs:
+            attrs['width'] = width
+        if 'height' not in attrs:
+            attrs['height'] = height        
         super(IFrame, self).__init__('iframe', attrs=attrs, parent=parent, child=child)
-        self.setup(parent, child)
 
 class Input(Element):
     class Type:
@@ -44,7 +44,7 @@ class Input(Element):
         hidden = "hidden"
         submit = "submit"
 
-    def __init__(self, name='', _type=Input.Type.text, value='', attrs=None, parent=None, child=None):
+    def __init__(self, name='', _type=Type.text, value='', attrs=None, parent=None, child=None):
         """
         Default type is 'text'.
         """
@@ -61,51 +61,46 @@ class Input(Element):
             if 'value' not in attrs:
                 attrs['value'] = value
 
-        super(Input, self).__init__('input', attrs=attrs, parent=parent, child=child)
-        self.setup(parent, child)
+        super(Input, self).__init__('input', attrs=attrs, parent=parent, child=child)        
 
 class AHref(Element):
-    def __init__(self, href='', text='', attrs=None, parent=None, child=None):
-        #attrs in attrs take precedence
-        if attrs is None or len(attrs) == 0:
-            if attrs is None:
-                attrs = {}            
-            if 'href' not in attrs:
-                attrs['href'] = href
-        
-        child_text = child
-        if child_text is None:
-            child_text = Element(text=text)
-                    
-        super(AHref, self).__init__('a', attrs=attrs, parent=parent, child=child)
-        self.setup(parent, child)
-
-class Img(Element):
-    def __init__(self, src='', width='20', height='20', attrs=None, parent=None, child=None):
+    def __init__(self, href='', text='', attrs=None, parent=None):
         #attrs in attrs take precedence
         if attrs is None or len(attrs) == 0:
             if attrs is None:
                 attrs = {}
+            if 'href' not in attrs:
+                attrs['href'] = href
+
+        if text is None:
+            text=''
+        text_child = Text(text=text)
+                    
+        super(AHref, self).__init__('a', attrs=attrs, parent=parent, child=text_child)        
+
+class Img(Element):
+    def __init__(self, src='', width='20', height='20', attrs=None, parent=None, child=None):
+        #attrs in attrs take precedence
+        if attrs is None:
+            attrs = {}
             
-            if 'src' not in attrs:
-                attrs['src'] = src
-            if 'width' not in attrs:
-                attrs['type'] = width
-            if 'height' not in attrs:
-                attrs['value'] = height
+        if 'src' not in attrs:
+            attrs['src'] = src
+        if 'width' not in attrs:
+            attrs['type'] = width
+        if 'height' not in attrs:
+            attrs['value'] = height
 
         super(Img, self).__init__('img', attrs=attrs, parent=parent, child=child)
-        self.setup(parent, child)
 
 class Label(Element):
     def __init__(self, text='', attrs=None, parent=None, child=None):
         #child element takes precedence over 'text' arg
-        child_text = child
-        if child_text is None:
-            child_text = Element(text=text)
+        text_child = child
+        if text_child is None:
+            text_child = Text(text=text)
 
-        super(Label, self).__init__('label', attrs=attrs, parent=parent, child=child)
-        self.setup(parent, child)
+        super(Label, self).__init__('label', attrs=attrs, parent=parent, child=text_child)        
 
 class Meta(Element):
     def __init__(self, name='', content='', attrs=None, parent=None, child=None):
@@ -119,33 +114,35 @@ class Meta(Element):
             if 'content' not in attrs:
                 attrs['content'] = content
 
-        super(Meta, self).__init__('meta', attrs=attrs, parent=parent, child=child)
-        self.setup(parent, child)
+        super(Meta, self).__init__('meta', attrs=attrs, self_closing=True, parent=parent, child=child)        
 
 class Title(Element):
     def __init__(self, title='', parent=None, child=None):
         #child element takes precedence over 'title' arg
-        child_text = child
-        if child_text is None:
-            child_text = Element(text=title)
+        text_child = child
+        if text_child is None:
+            text_child = Text(text=title)
         
-        super(Title, self).__init__('title', parent=parent, child=child_text)
-        self.setup(parent, child)
+        super(Title, self).__init__('title', parent=parent, child=text_child)        
 
 class Head(Element):
-    def __init__(self, parent=None, child=None):
-        super(Head, self).__init__('head', parent=parent, child=child)
-        self.setup(parent, child)
+    def __init__(self, text='',parent=None, child=None):
+        if child is None:
+            child = Text(text=text)
+        super(Head, self).__init__('head', parent=parent, child=child)        
 
 class Body(Element):
     def __init__(self, parent=None, child=None):
-        super(Body, self).__init__('body', parent=parent, child=child)
-        self.setup(parent, child)
+        super(Body, self).__init__('body', parent=parent, child=child)        
+
+class HTML(Element):
+    def __init__(self, child=None):
+        super(HTML, self).__init__('html', child=child)        
 
 class BR(Element):
     def __init__(self,parent=None, child=None):
-        super(BR, self).__init__('br', parent=parent, child=child)
-        self.setup(parent, child)
+        super(BR, self).__init__('br', parent=parent, child=child)        
     
-    def generate(self):
-        return '<{}>'.format(self.name)
+    def generate(self, indent_level=0):
+        indent = self.get_indent(indent_level)
+        return indent+'<{}>'.format(self.name)

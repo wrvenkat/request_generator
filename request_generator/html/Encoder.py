1e24b6ffdcba1e2ccbd336c73015fefa5a70e1f0
Venkat Raman <wrvenkat@gmail.com>
2019-07-20 20:49:57 -0400

    Added more output encoding schemes for different contexts and their tests.

--- request_generator/html/Encoder.py
+++ request_generator/html/Encoder.py
@@ -18,46 +18,194 @@ class Encoder:
         &, <, >, ", ', /
         """
 
-        input = cls._get_UTF8(input)
+        if len(input) == 0:
+            return ''
 
-        ampersand = cls._get_code_point('&')
-        less_than = cls._get_code_point('<')
-        grtr_than = cls._get_code_point('>')
-        dbl_quote = cls._get_code_point('"')
-        sgl_quote = cls._get_code_point("'")
-        fwd_slash = cls._get_code_point('/')
+        input = cls.get_UTF8(input)
+
+        ampersand = cls.get_code_point('&')
+        less_than = cls.get_code_point('<')
+        grtr_than = cls.get_code_point('>')
+        dbl_quote = cls.get_code_point('"')
+        sgl_quote = cls.get_code_point("'")
+        fwd_slash = cls.get_code_point('/')
 
         output_encoded_input = ''
         for char in input:
-            char_codepoint = cls._get_code_point(char)
-            replacement = char
+            char_codepoint = cls.get_code_point(char)
+            replacement = cls.get_UTF8(char)
 
             if (char_codepoint == ampersand):
-                replacement = cls._get_hex_entity(ampersand)
+                replacement = cls.get_hex_entity(ampersand)
             elif (char_codepoint == less_than):
-                replacement = cls._get_hex_entity(less_than)
+                replacement = cls.get_hex_entity(less_than)
             elif (char_codepoint == grtr_than):
-                replacement = cls._get_hex_entity(grtr_than)
+                replacement = cls.get_hex_entity(grtr_than)
             elif (char_codepoint == dbl_quote):
-                replacement = cls._get_hex_entity(dbl_quote)
+                replacement = cls.get_hex_entity(dbl_quote)
             elif (char_codepoint == sgl_quote):
-                replacement = cls._get_hex_entity(sgl_quote)
+                replacement = cls.get_hex_entity(sgl_quote)
             elif (char_codepoint == fwd_slash):
-                replacement = cls._get_hex_entity(fwd_slash)
+                replacement = cls.get_hex_entity(fwd_slash)
 
             output_encoded_input += replacement
         
         return output_encoded_input
     
     @classmethod
-    def encode_for_HTML_attrib(cls, input):
+    def encode_for_HTML_attrib(cls, input, quoted_context=False, single_quotes=False):
         """
         HTML Hex encodes for HTML attrib context.
+
+        Takes,
+            input = input string
+            quoted_context = whether output encoding needs to take place inside
+                a quoted_context or not. By default, no quoting is assumed.
+            single_quotes = if quoted_context is true, single_quotes when true
+                means enclosing context is inside single quotes (''). When false, it
+                means enclosing context is inside double quotes ("").
         """
-        pass
+        input = cls.get_UTF8(input)
+        if len(input) == 0:
+            return ''
+
+        single_quotes_context = False
+        if quoted_context and single_quotes:
+            single_quotes_context = True        
+        
+        encoded_output = ''
+        for char in input:
+            char_codepoint = cls.get_code_point(char)
+            replacement = cls.get_UTF8(char)
+
+            if quoted_context:
+                if single_quotes_context:
+                    single_quote_code_point = cls.get_code_point('\'')
+                    if single_quote_code_point == char_codepoint:
+                        replacement = cls.get_hex_entity(single_quote_code_point)
+                #double quotes context
+                else:
+                    double_quotes_code_point = cls.get_code_point('"')
+                    if double_quotes_code_point == char_codepoint:
+                        replacement = cls.get_hex_entity(double_quotes_code_point)
+            else:
+                # hex entity encode any character that's less than 256 and not
+                # alphanumeric in ASCII (includes extended ASCII as well)
+                # UTF-8 supports extended ASCII                            
+                small_a_code_point = Encoder.get_code_point('a')
+                small_z_code_point = Encoder.get_code_point('z')
+                caps_a_code_point = Encoder.get_code_point('A')
+                caps_z_code_point = Encoder.get_code_point('Z')
+                zero_code_point = Encoder.get_code_point(0)
+                nine_code_point = Encoder.get_code_point(9)
+
+                if not  ((char_codepoint >= small_a_code_point and char_codepoint <= small_z_code_point) or
+                        (char_codepoint >= caps_a_code_point and char_codepoint <= caps_z_code_point) or
+                        (char_codepoint >= zero_code_point and char_codepoint <= nine_code_point) and 
+                        char_codepoint < 256):
+                        replacement = cls.get_hex_entity(char_codepoint)
+
+            encoded_output += replacement
+       
+        return encoded_output
 
     @classmethod
-    def _get_hex_entity(cls, codepoint):
+    def encode_for_JS_data_values(cls, input):
+        """
+        Encodes input into \\xHH format for use in a JS data value context enclosed
+        in single quotes.
+
+        Examples of those contexts include event handler attributes,
+        JS variable assignment.
+        """
+        """
+        See: https://github.com/OWASP/CheatSheetSeries/blob/master/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.md#rule-3---javascript-escape-before-inserting-untrusted-data-into-javascript-data-values
+        """
+
+        input = cls.get_UTF8(input)
+
+        if len(input) == 0:
+            return ''
+        
+        encoded_output = ''
+        for char in input:
+            char_codepoint = cls.get_code_point(char)
+            replacement = cls.get_UTF8(char)
+
+            # \xHH encode any character that's less than 256 and not
+            # alphanumeric in ASCII (includes extended ASCII as well)
+            # UTF-8 supports extended ASCII            
+            small_a_code_point = Encoder.get_code_point('a')
+            small_z_code_point = Encoder.get_code_point('z')
+            caps_a_code_point = Encoder.get_code_point('A')
+            caps_z_code_point = Encoder.get_code_point('Z')
+            zero_code_point = Encoder.get_code_point(0)
+            nine_code_point = Encoder.get_code_point(9)
+
+            if not  ((char_codepoint >= small_a_code_point and char_codepoint <= small_z_code_point) or
+                    (char_codepoint >= caps_a_code_point and char_codepoint <= caps_z_code_point) or
+                    (char_codepoint >= zero_code_point and char_codepoint <= nine_code_point) and 
+                    char_codepoint < 256):
+                    replacement = unicode('\\x{:02X};'.format(char_codepoint))
+
+            encoded_output += replacement
+       
+        return encoded_output
+
+    @classmethod
+    def escape_for_css(cls, input):
+        """
+        Encodes input into \\xHH format for use CSS property value or 
+        HTML style property.
+
+        It's important that you only use untrusted data in a property value and not into other places in style data. You should stay away from putting untrusted data into complex properties like url, behavior, and custom (-moz-binding).
+        """
+        """
+        See https://github.com/OWASP/CheatSheetSeries/blob/master/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.md#rule-4---css-escape-and-strictly-validate-before-inserting-untrusted-data-into-html-style-property-values
+        """
+        return cls.encode_for_JS_data_values(input)
+
+    @classmethod
+    def escape_for_url_parameter_value(cls,input):
+        """
+        URL Escapes (%HH) input for safe use in URL parmater values.
+        """
+        """
+        See https://github.com/OWASP/CheatSheetSeries/blob/master/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.md#rule-5---url-escape-before-inserting-untrusted-data-into-html-url-parameter-values
+        """
+
+        input = cls.get_UTF8(input)
+
+        if len(input) == 0:
+            return ''
+        
+        encoded_output = ''
+        for char in input:
+            char_codepoint = cls.get_code_point(char)
+            replacement = cls.get_UTF8(char)
+
+            # \xHH encode any character that's less than 256 and not
+            # alphanumeric in ASCII (includes extended ASCII as well)
+            # UTF-8 supports extended ASCII            
+            small_a_code_point = Encoder.get_code_point('a')
+            small_z_code_point = Encoder.get_code_point('z')
+            caps_a_code_point = Encoder.get_code_point('A')
+            caps_z_code_point = Encoder.get_code_point('Z')
+            zero_code_point = Encoder.get_code_point(0)
+            nine_code_point = Encoder.get_code_point(9)
+
+            if not  ((char_codepoint >= small_a_code_point and char_codepoint <= small_z_code_point) or
+                    (char_codepoint >= caps_a_code_point and char_codepoint <= caps_z_code_point) or
+                    (char_codepoint >= zero_code_point and char_codepoint <= nine_code_point) and 
+                    char_codepoint < 256):
+                    replacement = unicode('%{:02X}'.format(char_codepoint))
+
+            encoded_output += replacement
+       
+        return encoded_output
+
+    @classmethod
+    def get_hex_entity(cls, codepoint):
         """
         Returns the hex entity equivalent for the codepoint.
         """
@@ -66,16 +214,26 @@ class Encoder:
             codepoint = int(codepoint)
             return unicode('&#x{:04X};'.format(codepoint))
         except ValueError:
-            raise TypeError("_getHexEntity(): Requires an input of type int.")    
+            raise TypeError("get_hex_entity(): Requires an input of type int.")    
 
     @classmethod
-    def _get_UTF8(cls, input):
+    def get_UTF8(cls, input=None):
         """
         Returns UTF-8 encoding of input.
         """
+        
+        if input is None:
+            raise TypeError("get_UTF8() takes atleast one argument. (0 given)")
 
-        if (len(input) == 0):
-            raise TypeError("_getUTF8() takes atleast one argument. (0 given)")
+        try:
+            input_length = len(input)
+        except TypeError:
+            # if len() doesn't apply to input, then
+            # try to get the str() of input
+            input_length = len(str(input))
+        
+        if input_length == 0:
+            raise TypeError("get_UTF8() takes atleast one argument. (0 given)")
 
         #if the input is not of UNICODE type,
         #then we transform it to unicode
@@ -87,25 +245,18 @@ class Encoder:
         return input
 
     @classmethod
-    def _get_code_point(cls,input):
+    def get_code_point(cls,input):
         """
         Returns the NFC normalized codepoint for the input character.
 
         Raises TypeError.
         """
-        
-        if (len(input) == 0):
-            return 0
 
-        #if the input is not of UNICODE type,
-        #then we transform it to unicode
-        if not isinstance(input, unicode):
-            #convert the input to bytes and
-            #decode that as UTF-8
-            input = bytes(input).decode('UTF-8')
+        #get UTF-8 value of input
+        input = cls.get_UTF8(input)
         
         if (len(input) > 1):
-            raise TypeError("getCodePoint() takes exactly one argument. ({} given)".format(len(input)))
+            raise TypeError("get_code_point() takes exactly one argument. ({} given)".format(len(input)))
 
         codepoint = unicodedata.normalize('NFC', input)
         codepoint = ord(input)        
--- request_generator/html/tests/Encoder.py
+++ request_generator/html/tests/Encoder.py
@@ -4,32 +4,38 @@ import unittest
 
 class EncoderTest(unittest.TestCase):
 
-    def test_a_getUTF8(self):
+    def test_a_get_UTF8(self):
         """
         Tests _getUTF8.
         """
+        
+        #empty input
+        with self.assertRaises(TypeError):
+            Encoder.get_UTF8('')
+        with self.assertRaises(TypeError):
+            Encoder.get_UTF8()
 
         #input is an str that's valid in
         #Windows-1252 alphabets
         #See: https://en.wikipedia.org/wiki/Windows-1252
         non_utf8_input_str = 'ÀÁÂÃÄÅ'
-        utf8_str = Encoder._get_UTF8(non_utf8_input_str)
+        utf8_str = Encoder.get_UTF8(non_utf8_input_str)
         self.assertTrue(isinstance(utf8_str, unicode))
 
         utf8_compatible_str = u'Ångström'
-        utf8_str = Encoder._get_UTF8(utf8_compatible_str)
+        utf8_str = Encoder.get_UTF8(utf8_compatible_str)
         self.assertTrue(isinstance(utf8_str, unicode))
 
-        with self.assertRaises(TypeError):
-            Encoder._get_UTF8('')
+        #input that's not a value str or bytes object
+        self.assertEqual(u'90909', Encoder.get_UTF8(90909))
     
-    def test_b_getHexEntity(self):
+    def test_b_get_hex_entity(self):
         """
         Tests _getCodePoint and getHexEntity.
         """
 
-        codepoint = Encoder._get_code_point(u'ç')
-        self.assertEquals(u'&#x00E7;', Encoder._get_hex_entity(codepoint))
+        codepoint = Encoder.get_code_point(u'ç')
+        self.assertEquals(u'&#x00E7;', Encoder.get_hex_entity(codepoint))
 
     def test_c_encode_for_HTML_content(self):
         
@@ -41,5 +47,37 @@ class EncoderTest(unittest.TestCase):
         self.assertEquals(u'&#x002F;&#x003E;&#x003C;a href=',
             Encoder.encode_for_HTML_content(input_2))
         self.assertEquals(u'javascript:&#x002F;*--&#x003E;&#x003C;&#x002F;title&#x003E;&#x003C;&#x002F;style&#x003E;&#x003C;&#x002F;textarea&#x003E;&#x003C;&#x002F;script&#x003E;&#x003C;&#x002F;xmp&#x003E;&#x003C;svg&#x002F;onload=&#x0027;+&#x002F;&#x0022;&#x002F;+&#x002F;onmouseover=1&#x002F;+&#x002F;[*&#x002F;[]&#x002F;+alert(1)&#x002F;&#x002F;&#x0027;&#x003E;', Encoder.encode_for_HTML_content(xss_locator))
+
+    def test_c_encode_for_HTML_attrib(self):
+        """
+        Tests encode_for_HTML_attrib.
+        """
+
+        attrib_single_quote = "'post context' text ®¾"
+        attrib_dbl_quote = '"post context text ®¾"'
+        attrib_no_quotes = '"post context text ®¾" asda\'asda\''
+                
+        self.assertEqual(u'&#x0027;post context&#x0027; text ®¾',Encoder.encode_for_HTML_attrib(attrib_single_quote, quoted_context=True, single_quotes=True))
+        self.assertEqual(u'&#x0022;post context text ®¾&#x0022;',Encoder.encode_for_HTML_attrib(attrib_dbl_quote, quoted_context=True))
+        self.assertEqual(u'&#x0022;post&#x0020;context&#x0020;text&#x0020;&#x00AE;&#x00BE;&#x0022;&#x0020;asda&#x0027;asda&#x0027;',Encoder.encode_for_HTML_attrib(attrib_no_quotes))
+
+    def test_d_encode_for_JS_data_values(self):
+        """
+        Tests encode_for_JS_data_values() and escape_for_css().
+        """
+        val1 = "alert('test xss')"
+        val2 = "javascript:alert(1)"
+        val3 = "location.href='http://attacker.com'"
+
+        self.assertEqual(u'alert\\x28;\\x27;test\\x20;xss\\x27;\\x29;', Encoder.encode_for_JS_data_values(val1))
+        self.assertEqual(u'javascript\\x3A;alert\\x28;1\\x29;', Encoder.encode_for_JS_data_values(val2))
+        self.assertEqual(u'location\\x2E;href\\x3D;\\x27;http\\x3A;\\x2F;\\x2F;attacker\\x2E;com\\x27;', Encoder.encode_for_JS_data_values(val3))
+    
+    def test_e_escape_for_url_parameters(self):
+        """
+        Tests escape_for_url_parameters.
+        """
+        url_param_value = 'title<script>alert("XSS injected")</script>'
+        self.assertEqual(u'title%3Cscript%3Ealert%28%22XSS%20injected%22%29%3C%2Fscript%3E', Encoder.escape_for_url_parameter_value(url_param_value))
 
 unittest.main()

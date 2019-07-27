02185ae24ae88612b3aa9d3f1cc370dcb3f9f81c
Venkat Raman <wrvenkat@gmail.com>
2019-07-27 18:44:52 -0400

    Added indentations to template.

--- request_generator/html/js_statements_template.py
+++ request_generator/html/js_statements_template.py
@@ -13,11 +13,11 @@ MULTI_PART_FILE_INPUT_ASSIGNMENT_STMT_2 = """fileToUpload{}.files = getFile({}, 
 #used to generate getFile JS function
 GET_FILE_FUNCTION_HEADER        = 'function getFile(file_content, file_name, content_type) {'
 GET_FILE_FUNCTION_FOOTER        = '}'
-JS_COMMENT                      = '// Firefox < 62 workaround exploiting https://bugzilla.mozilla.org/show_bug.cgi?id=1422655'
-DATA_TRANSFER_JS_CLIPBOARD_STMT = """const dataTransfer = new ClipboardEvent('').clipboardData || """
-DATA_TRANSFER_JS_STMT           = 'new DataTransfer(); // specs compliant (as of March 2018 only Chrome)'
-IF_CONDN                        = 'if (content_type != undefined)'
-IF_TRUE                         = 'dataTransfer.items.add(new File([file_content], file_name, {type : content_type}));'
-ELSE_STMT                       = 'else'
-IF_FALSE                        = 'dataTransfer.items.add(new File([file_content], file_name));'
-RETURN_STMT                     = 'return dataTransfer.files;'
+JS_COMMENT                      = '\t// Firefox < 62 workaround exploiting https://bugzilla.mozilla.org/show_bug.cgi?id=1422655'
+DATA_TRANSFER_JS_CLIPBOARD_STMT = """\tconst dataTransfer = new ClipboardEvent('').clipboardData || """
+DATA_TRANSFER_JS_STMT           = '\tnew DataTransfer(); // specs compliant (as of March 2018 only Chrome)'
+IF_CONDN                        = '\tif (content_type != undefined)'
+IF_TRUE                         = '\t\tdataTransfer.items.add(new File([file_content], file_name, {type : content_type}));'
+ELSE_STMT                       = '\telse'
+IF_FALSE                        = '\t\tdataTransfer.items.add(new File([file_content], file_name));'
+RETURN_STMT                     = '\treturn dataTransfer.files;'

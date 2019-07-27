6ca6b30e71a5167caf1428961b39beaf326e9ad5
Venkat Raman <wrvenkat@gmail.com>
2019-07-27 18:36:29 -0400

    Added utils package with utils module.

---
+++ request_generator/utils/utils.py
@@ -0,0 +1,27 @@
+from os.path import isabs, dirname, abspath, isdir, join, normpath
+import inspect
+
+def get_abs_path(*dirs):
+    """
+    Return absolute path of a directory constructed by appending dirs one after another
+    dirs are all directories under the module 'request_parser'.
+
+    if the last dir of dirs provided has a '/', then no additional '/' is added.
+
+    Ex: get_abs_path("dir1", "subdir") returns /home/user/Downloads/request_parser/dir1/subdir/
+    """
+    #for relative path
+    curr_filename = inspect.getframeinfo(inspect.currentframe()).filename
+    curr_path = dirname(abspath(curr_filename))
+    previous_path = join(curr_path, "../")
+    previous_path = normpath(previous_path)
+    curr_path = previous_path
+
+    if dirs:
+        for dir in dirs:
+            curr_path = join(curr_path, dir)
+    curr_path = join(curr_path,"")
+    curr_path = normpath(curr_path)
+
+    #return the current dir if it's a directory
+    return curr_path

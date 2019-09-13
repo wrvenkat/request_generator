dd98070f9e91c1f0691ed16ab6e9fd1b4e15a1fd
Venkat Raman <wrvenkat@gmail.com>
2019-09-13 08:29:20 -0400

    Added initial travis support.

---
+++ .travis/run-jython-tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+jython_path=~/Downloads/Jython/jython.jar;
+java -jar "$jython_path" -m request_generator.html.dom.tests.simple_html_element &&\
+java -jar "$jython_path" -m request_generator.html.tests.html_request_builder &&\
+java -jar "$jython_path" -m request_generator.html.jquery.tests.jquery_request_builder

1cb8fa1652a013ac284e5ac647edafa8c9919547
Venkat Raman <wrvenkat@gmail.com>
2019-09-13 20:29:36 -0400

    Added install for request_parser.

--- .travis/run-jython-tests.sh
+++ .travis/run-jython-tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 jython_path=~/Downloads/Jython/jython.jar;
+git clone https://github.com/wrvenkat/request_parser.git &&
+pwd &&\
+mv /Users/travis/build/wrvenkat/request_parser/request_parser /Users/travis/build/wrvenkat/request_generator/ &&\
 java -jar "$jython_path" -m request_generator.html.dom.tests.simple_html_element &&\
 java -jar "$jython_path" -m request_generator.html.tests.html_request_builder &&\
 java -jar "$jython_path" -m request_generator.html.jquery.tests.jquery_request_builder

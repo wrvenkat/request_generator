e916d8fc2f15a906cf08a16b754e3d0ca1d16cf9
Venkat Raman <wrvenkat@gmail.com>
2019-09-13 22:29:45 -0400

    Added missing test.

--- .travis/run-jython-tests.sh
+++ .travis/run-jython-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 jython_path=~/Downloads/Jython/jython.jar;
 java -jar "$jython_path" -m request_generator.html.dom.tests.simple_html_element &&\
+java -jar "$jython_path" -m request_generator.html.dom.tests.simple_html_elements &&\
 java -jar "$jython_path" -m request_generator.html.tests.html_request_builder &&\
 java -jar "$jython_path" -m request_generator.html.jquery.tests.jquery_request_builder
